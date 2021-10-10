ccd98542fdbb6c6de143b15d97a383ceac9aeb2f
Eduard Draghiciu <eduarddr96@gmail.com>
2021-10-10 11:08:07 -0400

    Add routing

---
+++ src/Components/App/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Navbar from '../Navbar/Navbar';
+export default function App(props) {
+	return (
+		<div className="App">
+			<Navbar />
+		</div>
+	);
+}
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,28 @@
 import './Navbar.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Userlist from '../Userlist/Userlist';
+import Addresslist from '../Addresslist/Addresslist';
+import User from '../User/User';
+import Home from '../Home/Home';
 
 export default function Navbar(props) {
-	return <nav>navigation</nav>;
+	return (
+		<>
+			<Switch>
+				<Route path="/users">
+					<Userlist />
+				</Route>
+				<Route path="/users/:id">
+					<User />
+				</Route>
+				<Route path="/addresses">
+					<Addresslist />
+				</Route>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Redirect to="/" />
+			</Switch>
+		</>
+	);
 }
